f775fed388e6c861b57e4a96414278c432844eda
jose19962014 <joseaorozco96@gmail.com>
2022-08-10 13:43:51 -0600

    se unio validaciones de los demas

---
+++ Frontend/js/contactUsController.js
@@ -0,0 +1,165 @@
+// inputs
+let formnombre = document.querySelector('#formnombre');
+let formapellidos = document.querySelector('#formapellidos');
+let formtelefono = document.querySelector('#formtelefono');
+let formemail = document.querySelector('#formemail');
+
+//botones
+let btnregistrar = document.querySelector('#btnregistrar');
+
+
+
+//funciones del form
+
+function alerttexto(texto){
+    Swal.fire({
+        title: 'Formato de texto erroneo',
+        text: texto,
+        icon: 'warning'
+        })
+}
+
+
+function  validationJustTextNum(texto){
+    const pattern = /^[A-Z0-9\s]+$/gi;
+    return  pattern.test(texto);
+
+}
+
+function  validationJusttelefono(numero){
+    const pattern = /(\d{4}\d{1,4})+$/g;
+     return pattern.test(numero);;
+ }
+
+	
+function validationJustemail( email ){
+    const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
+    return expression.test(String(email).toLowerCase())
+}
+function limpiarForm(){
+    nombre.value = '';
+    apellidos.value = '';
+    cedula.value = '';
+    direccion.value = '';
+    telefono.value = '';
+    password.value = '';
+    email.value = '';
+  
+  
+    nombre.style.borderColor = 'white';
+    apellidos.style.borderColor = 'white';
+    cedula.style.borderColor = 'white';
+    direccion.style.borderColor = 'white';
+    telefono.style.borderColor = 'white';
+    password.style.borderColor = 'white';
+
+}
+
+
+function validacionformnombre(){
+     let textformnombre = formnombre.value;
+    if(!validationJustTextNum(textformnombre)){
+        alerttexto('Nombre solo admite letras y no puede estar vacio')
+        formnombre.style.borderColor = 'red';
+        return;
+    }
+    else{
+        formnombre.style.borderColor = 'palegreen';
+        return true;
+    }
+}
+
+function validacionformapellidos(){
+    let textformapellidos = formapellidos.value;
+    if(!validationJustTextNum(textformapellidos)){
+        alerttexto('Apellidos admite letras y no puede estar vacio')
+        formapellidos.style.borderColor = 'red';
+        return;
+    } 
+    else{
+        formapellidos.style.borderColor = 'palegreen';
+        return true;
+    }
+
+}
+
+
+function validacionformtelefono(){
+    let numformtelefono = formtelefono.value;
+    if(!validationJusttelefono(numformtelefono)){
+        alerttexto('Numero de teléfono solo admite numeros y no puede estar vacio')
+        formtelefono.style.borderColor = 'red';
+        return;
+  }
+    else{
+    telefono.style.borderColor = 'palegreen';
+    return true;
+  }
+
+}
+
+function validacionformemail(){
+    let textformemail =  formemail.value;
+    if(!validationJustemail(textformemail)){
+        alerttexto('El correo electrónico debe de ingresarlo por completo')
+        formemail.style.borderColor = 'red';
+        return;
+    }   
+ 
+    else{
+        direccion.style.borderColor = 'palegreen';
+        return true;
+    }
+
+    
+}
+
+
+function validacionDeData (){
+    validacionformnombre();
+    validacionformapellidos();
+    validacionformcedula();
+    validacionformtelefono();
+    validacionformdireccion();
+    validacionformemail();
+    validacionformpassword();
+    
+    if(validacionformnombre()){
+        if(validacionformapellidos()){
+            if(validacionformcedula()){
+                if(validacionformdireccion()){
+                    if(validacionformemail()){
+                        if(validacionformpassword()){
+                        return true
+                        }else{return false;}
+                    }else{return false;}
+                }else{return false;}
+            }else{return false;}
+        }else{return false;}
+     }else{return false;}
+   
+}
+
+
+
+btnregistrar.addEventListener("click",function(){
+
+    if(validacionDeData()){
+
+        const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+        })
+            
+            limpiarForm();
+    }
+})
+
+
+///funciones de la tabla
+function getExpedineteById(index){
+    butFormCrear.disabled = true;
+}
---
+++ Frontend/js/hotelReservacionController.js
@@ -0,0 +1,237 @@
+// inputs
+let formNombreMascota = document.querySelector('#formNombreMascota');
+let formNombreDueño = document.querySelector('#formNombreDueño');
+let formCedula = document.querySelector('#formCedula');
+let formFechaIngreso = document.querySelector('#formFechaIngreso');
+let formFechaSalida = document.querySelector('#formFechaSalida');
+//botones
+let butformActualizar = document.querySelector('#butFormActualizar');
+let butFormCrear = document.querySelector('#butFormCrear');
+let butFormCancel = document.querySelector('#butFormCancel');
+//tabla
+let tableRecord =  document.querySelector('#tableRecord');
+let butTableActualizar = document.querySelector('#butTableActualizar');
+let butTableBorrar = document.querySelector('#butTableBorrar');
+
+
+
+//funciones del form
+
+function alerttexto(texto){
+    Swal.fire({
+        title: 'Formato de texto erroneo',
+        text: texto,
+        icon: 'warning'
+        })
+}
+
+
+function  validationJustTextNum(texto){
+    const pattern = /^[A-Z0-9\s]+$/gi;
+    return  pattern.test(texto);
+}
+
+function  validationJustCedula(numero){
+   const pattern = /(\d{1}-\d{1,4}-\d{1,4})+$/g;
+    return pattern.test(numero);;
+}
+
+function limpiarForm(){
+    formNombreMascota.value = '';
+    formNombreDueño.value = '';
+    formCedula.value = '';
+    formDireccion.value = '';
+    formDoctor.value = 0;
+    formFechaIngreso.value = "";
+    formFechaSalida.value = "";
+  
+  
+    formNombreMascota.style.borderColor = 'white';
+    formNombreDueño.style.borderColor = 'white';
+    formCedula.style.borderColor = 'white';
+    formDireccion.style.borderColor = 'white';
+    formDoctor.style.borderColor = 'white';
+    formFechaIngreso.style.borderColor = 'white';
+    formFechaSalida.style.borderColor = 'white';
+
+    butFormCrear.disabled = false;
+}
+
+
+function validacionNombreMascota(){
+     let textformNombreMascota = formNombreMascota.value;
+    if(!validationJustTextNum(textformNombreMascota)){
+        alerttexto('Nombre de la mascota solo admite letras y no puede estar vacio')
+        formNombreMascota.style.borderColor = 'red';
+        return;
+    }
+    else{
+        formNombreMascota.style.borderColor = 'palegreen';
+        return true;
+    }
+}
+
+function validacionNombreDueno(){
+    let textformNombreDueño = formNombreDueño.value;
+    if(!validationJustTextNum(textformNombreDueño)){
+        alerttexto('Nombre del dueno solo admite letras y no puede estar vacio')
+        formNombreDueño.style.borderColor = 'red';
+        return;
+    } 
+    else{
+        formNombreDueño.style.borderColor = 'palegreen';
+        return true;
+    }
+}
+
+function validacionCedula(){
+    let numFormCedula =  formCedula.value;
+    if(!validationJustCedula(numFormCedula)){
+        alerttexto('Numero de cedula solo admite numeros y no puede estar vacio')
+        formCedula.style.borderColor = 'red';
+        return;
+    }
+ 
+    else{
+        formCedula.style.borderColor = 'palegreen';
+        return true;
+    }
+}
+
+
+
+
+function validacionDoctor(){
+  let textformDoctor = formDoctor.value;
+  if(textformDoctor == "0"){
+        alerttexto('El campo doctor es obligatorio')
+        formDoctor.style.borderColor = 'red';
+        return;
+  }
+  else{
+    formDoctor.style.borderColor = 'palegreen';
+    return true;
+  }
+}
+
+function validacionFechaEntrada(){
+    let textformFechaIngreso = formFechaIngreso.value; 
+
+    if(textformFechaIngreso != "" && textformFechaIngreso != null)
+    {
+        const maxDate = new Date();
+        maxDate.setDate(maxDate.getDate()+15); 
+
+        const dateSelecionado = new Date(textformFechaIngreso)
+        if(dateSelecionado > maxDate){
+            alerttexto('No se puede reservar con mas de 15 dias de anticipacion')
+            formFechaIngreso.style.borderColor = 'red';
+            return;
+        }
+        else{
+            formFechaIngreso.style.borderColor = 'palegreen';
+            return true;
+        }
+    }
+    else{
+        alerttexto('La fecha de ingreso es obligatoria')
+        formFechaIngreso.style.borderColor = 'red';
+        return;        
+    }
+}
+
+function validacionFechaSalida(){
+    let textformFechaSalida = formFechaSalida.value; 
+
+    if(textformFechaSalida != "" && textformFechaSalida != null)
+    {
+
+        formFechaSalida.style.borderColor = 'palegreen';
+        return true;
+        
+    }
+    else{
+        alerttexto('La fecha de salida es obligatoria')
+        formFechaSalida.style.borderColor = 'red';
+        return;        
+    }
+}
+
+function validacionDeData (){
+
+
+
+     if(validacionNombreMascota()){
+        if(validacionNombreDueno()){
+            if(validacionCedula()){
+                
+                    if(validacionFechaEntrada()){
+                        
+                            if(validacionFechaSalida()){
+                                return true
+                            }else{return false;}
+                        }else{return false;}
+                  
+                
+            }else{return false;}
+        }else{return false;}
+    }else{return false;}
+}
+
+
+///funciones de la tabla
+function getExpedineteById(index){
+    butFormCrear.disabled = true;
+}
+
+
+
+//botones del form
+butFormCancel.addEventListener('click',function(){
+
+    limpiarForm();  
+})
+
+butFormCrear.addEventListener("click",function(){
+if(validacionDeData()){
+
+        const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+        })
+
+        swalWithBootstrapButtons.fire({
+        title: 'El Moton a pagar?',
+        text: "El moton a pgar son 75000 colones",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si, Continuar',
+        cancelButtonText: 'No',
+        reverseButtons: true
+        }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "/Frontend/pantallaPago.html"
+        } else if (
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            
+            limpiarForm();
+        }
+        })
+
+    }
+})
+
+butformActualizar.addEventListener("click",function(){
+    limpiarForm();
+    validacionDeData();
+})
+
+
+
+
+
+
